252ba31031a12ca99bca38c9d4089be2c7d3d99c
Denieall <denieall.joe@gmail.com>
2018-07-28 12:10:15 +0800

    Add Room Database

--- app/src/main/java/com/denieall/crud/Model/User.java
+++ app/src/main/java/com/denieall/crud/Model/User.java
@@ -1,17 +1,55 @@
 package com.denieall.crud.Model;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+
+// Adding @Entity says that this class will be table saving data in the database
+@Entity
 public class User {
 
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "first_name")
     private String first_name;
+
+    @ColumnInfo(name = "last_name")
     private String last_name;
+
+    @ColumnInfo(name = "email")
     private String email;
 
+    // Constructor
     public User(String first_name, String last_name, String email) {
 
         this.first_name = first_name;
         this.last_name = last_name;
         this.email = email;
 
+    }
+
+
+    // Getters and setters --- basically getting and setting from the database
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFirst_name() {
